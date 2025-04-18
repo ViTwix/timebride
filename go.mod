24a68cc2309cb002c2b4d8a2304e4f6da9fe1209
Vitalii Povolotskyi <vitwix@Twix-MacBook-Pro.local>
2025-04-19 02:29:16 +0300

    cursor_v0.03

--- go.mod
+++ go.mod
@@ -1,21 +1,45 @@
 module timebride
 
-go 1.21
+go 1.23.0
+
+toolchain go1.24.2
 
 require (
+	github.com/aws/aws-sdk-go-v2 v1.25.3
+	github.com/aws/aws-sdk-go-v2/config v1.27.7
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.7
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.51.4
 	github.com/golang-jwt/jwt/v5 v5.2.0
 	github.com/golang-migrate/migrate/v4 v4.17.1
-	github.com/google/uuid v1.5.0
+	github.com/google/uuid v1.6.0
 	github.com/gorilla/mux v1.8.1
+	github.com/joho/godotenv v1.5.1
 	github.com/lib/pq v1.10.9
 	github.com/spf13/viper v1.18.2
+	github.com/stretchr/testify v1.10.0
 	golang.org/x/crypto v0.31.0
 	gorm.io/datatypes v1.2.0
 	gorm.io/driver/postgres v1.5.4
-	gorm.io/gorm v1.25.5
+	gorm.io/driver/sqlite v1.5.7
+	gorm.io/gorm v1.25.12
 )
 
 require (
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.1 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.15.3 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.3 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.3 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.3.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.3.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.17.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.20.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.23.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.28.4 // indirect
+	github.com/aws/smithy-go v1.20.1 // indirect
+	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/fsnotify/fsnotify v1.7.0 // indirect
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
@@ -28,8 +52,10 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/magiconair/properties v1.8.7 // indirect
+	github.com/mattn/go-sqlite3 v1.14.28 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.1.0 // indirect
+	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
 	github.com/sagikazarmark/locafero v0.4.0 // indirect
 	github.com/sagikazarmark/slog-shim v0.1.0 // indirect
 	github.com/sourcegraph/conc v0.3.0 // indirect
@@ -40,9 +66,9 @@ require (
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/multierr v1.9.0 // indirect
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9 // indirect
-	golang.org/x/sync v0.10.0 // indirect
+	golang.org/x/sync v0.13.0 // indirect
 	golang.org/x/sys v0.28.0 // indirect
-	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	gorm.io/driver/mysql v1.4.7 // indirect
--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
 )
 
@@ -11,6 +12,7 @@ type Config struct {
 	Server   ServerConfig   `mapstructure:"server"`
 	Database DatabaseConfig `mapstructure:"database"`
 	JWT      JWTConfig      `mapstructure:"jwt"`
+	Storage  StorageConfig  `mapstructure:"storage"`
 }
 
 // ServerConfig містить налаштування сервера
@@ -37,12 +39,61 @@ type JWTConfig struct {
 	TokenDuration time.Duration `mapstructure:"token_duration"`
 }
 
+// StorageConfig містить налаштування сховища файлів
+type StorageConfig struct {
+	Provider  string          `mapstructure:"provider"`
+	Backblaze BackblazeConfig `mapstructure:"backblaze"`
+	CDN       CDNConfig       `mapstructure:"cdn"`
+}
+
+// BackblazeConfig містить налаштування Backblaze B2
+type BackblazeConfig struct {
+	AccountID      string `mapstructure:"account_id"`
+	ApplicationKey string `mapstructure:"application_key"`
+	Bucket         string `mapstructure:"bucket"`
+	BucketID       string `mapstructure:"bucket_id"`
+	Endpoint       string `mapstructure:"endpoint"`
+	Region         string `mapstructure:"region"`
+}
+
+// CDNConfig містить налаштування CDN
+type CDNConfig struct {
+	Enabled  bool   `mapstructure:"enabled"`
+	Domain   string `mapstructure:"domain"`
+	Protocol string `mapstructure:"protocol"`
+}
+
 // Load завантажує конфігурацію з файлу
 func Load() (*Config, error) {
+	// Спочатку завантажуємо .env файл, перевіряючи різні можливі шляхи
+	envPaths := []string{
+		".env",
+		"../.env",
+		"../../.env",
+		"../../../.env",
+		"./config/.env",
+		"../config/.env",
+		"../../config/.env",
+	}
+
+	var envLoaded bool
+	for _, path := range envPaths {
+		if err := godotenv.Load(path); err == nil {
+			envLoaded = true
+			break
+		}
+	}
+
+	if !envLoaded {
+		return nil, godotenv.Load() // спробуємо завантажити з поточної директорії як останній варіант
+	}
+
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(".")
 	viper.AddConfigPath("./config")
+	viper.AddConfigPath("../..")
+	viper.AddConfigPath("../../config")
 
 	// Встановлюємо значення за замовчуванням
 	viper.SetDefault("server.port", 8080)
@@ -53,9 +104,23 @@ func Load() (*Config, error) {
 	viper.SetDefault("database.port", 5432)
 	viper.SetDefault("database.sslmode", "disable")
 	viper.SetDefault("jwt.token_duration", "24h")
+	viper.SetDefault("storage.backblaze.endpoint", "https://s3.eu-central-003.backblazeb2.com")
+	viper.SetDefault("storage.backblaze.region", "eu-central-003")
 
+	// Зв'язуємо змінні середовища з конфігурацією
+	viper.BindEnv("storage.backblaze.account_id", "B2_ACCOUNT_ID")
+	viper.BindEnv("storage.backblaze.application_key", "B2_APPLICATION_KEY")
+	viper.BindEnv("storage.backblaze.bucket", "B2_BUCKET")
+	viper.BindEnv("storage.backblaze.bucket_id", "B2_BUCKET_ID")
+
+	// Завантажуємо змінні середовища
+	viper.AutomaticEnv()
+
+	// Завантажуємо основний конфіг
 	if err := viper.ReadInConfig(); err != nil {
-		return nil, err
+		if _, ok := err.(viper.ConfigFileNotFoundError); !ok {
+			return nil, err
+		}
 	}
 
 	var config Config
